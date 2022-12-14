c846854968cfc32367b7888455f492cfe6f5bff5
gbogom <gbogom@gmail.com>
2022-12-13 19:20:38 -0500

    primer commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app= express();
+const path = require('path');
+app.use(express.static('public'));
+
+app.listen(3030,()=>console.log("Server running http://localhost:3030"))
+
+
+let home = path.resolve(__dirname,'./views/home.html');
+let register = path.resolve(__dirname,'./views/register.html');
+let login = path.resolve(__dirname,'./views/login.html');
+
+app.get('/', function(req,res){
+    res.sendFile(home);
+})
+
+app.get('/register', function(req,res){
+    res.sendFile(register);
+})
+
+app.get('/login', function(req,res){
+    res.sendFile(login);
+})
